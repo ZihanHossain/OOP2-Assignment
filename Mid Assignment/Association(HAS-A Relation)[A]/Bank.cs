34e5e1ebf9b32bd6cd878c5682ffbef1fc32c1f4
zihan <zihanhossain02@gmail.com>
2021-02-27 20:39:37 +0600

    withdraw_deposite

---
+++ Mid Assignment/Association(HAS-A Relation)[A]/Bank.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Association_HAS_A_Relation__A_
+{
+    class Bank
+    {
+        private string name;
+        private Account[] accounts;//1-* Relation
+        public Bank(string name,int size)
+        {
+            this.name = name;
+            accounts = new Account[size];
+        }
+        public string Name
+        {
+            set { this.name = value; }
+            get { return this.name; }
+        }
+        public Account[] Accounts
+        {
+            set { this.accounts = value; }
+            get { return this.accounts; }
+        }
+
+        public void PrintAllAccounts()
+        {
+            for(int i=0;i<accounts.Length;i++)
+            {
+                if (accounts[i] == null)
+                {
+                    continue;
+                }
+                accounts[i].PrintAccount();
+            }
+        }
+
+        public void AddAccount(Account account)
+        {
+            for (int i = 1; i <= accounts.Length; i++)
+            {
+                if(accounts[i]==null)
+                {
+                    account.AccountNumber = i;
+                    accounts[i] = account;
+                    break;
+                }
+            }
+        }
+
+        public void SearchAccount(int accountNo)
+        {
+            int flag = 0;
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                if (accounts[i] == null)
+                {
+                    continue;
+                }
+                else if (accounts[i].AccountNumber == accountNo)
+                {
+                    accounts[i].PrintAccount();
+                    flag = 0;
+                    break;
+                }
+                else
+                {
+                    flag = 1;
+                    
+                }
+            }
+            if(flag==1)
+                Console.WriteLine("Account Not Found");
+        }
+
+        public void DeleteAccout(int accountNo)
+        {
+            int flag = 0;
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                if (accounts[i] == null)
+                {
+                    continue;
+                }
+                else if (accounts[i].AccountNumber == accountNo)
+                {
+                    flag = 0;
+                    for (int j = i; j < (accounts.Length)-1; j++)
+                    {
+                        accounts[j] = accounts[j+1];
+
+                        if (j == (accounts.Length)-2)
+                        {
+                         accounts[j] = null;
+                        }
+                    }
+                }
+                else
+                {
+                    flag = 1;
+                }
+            }
+            if(flag==1)
+                Console.WriteLine("account not found");
+        }
+
+        public void Transaction(int type, int accountNumber, )
+        {
+            
+        }
+    }
+}
---
+++ Mid Assignment/Association(HAS-A Relation)[A]/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Association_HAS_A_Relation__A_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Address address1 = new Address(4,10,"Dhaka");
+            //Account account1 = new Account(1001, "Shakib", 2000, address1);
+            //account1.PrintAccount();
+
+            //Account account2 = new Account(1002,"Tamim",3000,new Address(7,20,"Chittagong"));
+            //account2.PrintAccount();
+
+            Bank ourBank = new Bank("Developer's bank", 5);
+            ourBank.AddAccount(new Account("Shakib", 2000, new Address(4, 10, "Dhaka" , "Bangladesh")));
+            ourBank.AddAccount(new Account("Mushfiq", 5000, new Address(4, 10, "Sylhet" , "Bangladesh")));
+            ourBank.AddAccount(new Account("Tamim", 3000, new Address(7, 20, "Chittagong" , "Bangladesh")));
+            //ourBank.PrintAllAccounts();
+            //ourBank.SearchAccount(300);
+            ourBank.DeleteAccout(3003);
+            ourBank.PrintAllAccounts();
+        }
+    }
+}
